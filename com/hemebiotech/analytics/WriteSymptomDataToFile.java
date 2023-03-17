5bb4a7e292df59a0e7f50212b1f68e23615876be
Steeve Victorin <vl1fjv3lbkw2-student@anonymous-codio.com>
2023-03-17 07:56:20 +0000

    writer implementation

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,8 +1,21 @@
 package com.hemebiotech.analytics;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class WriteSymptomDataToFile implements ISymptomWriter {
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		try {
+			FileWriter writer = new FileWriter(new File("result.out"));
+			for (String symptom : symptoms.keySet()) {
+				writer.write(symptom + ": " + symptoms.get(symptom) + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+  
